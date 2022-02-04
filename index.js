eb580cb7775eba166e191487073b9edf89257faa
Leissan Khan <leissan.khan@gmail.com>
2022-02-04 12:01:33 -0500

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 function saturdayFun (activity ='roller-skate') {
     console.log(`This Saturday, I want to ${activity}!`);
 }
+saturdayFun("bathe my dog");
+const mondayWork= function(work="go to the office") {
+    console.log(`This Monday, I will ${work}`);
+}
+mondayWork("work from home");
+
+
+function wrapAdjective() {
+    
+}
